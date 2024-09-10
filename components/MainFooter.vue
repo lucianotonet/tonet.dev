2d7792f406fb579c015f8eb7dd2665afa500fda0
Luciano T. <tonetlds@gmail.com>
2024-09-10 17:22:33 -0300

    Melhorar design do rodapé com suporte a modos claro/escuro
    Atualizado o rodapé para incluir um novo layout responsivo e suporte a temas claro e escuro, utilizando uma imagem de contribuição do GitHub que se adapta ao modo atual. Essa alteração melhora a estética e a usabilidade, garantindo que o rodapé permaneça visualmente atraente e informativo em todas as condições de exibição. Além disso, foram introduzidos gradientes de fundo que aprimoram a qualidade visual. Isso contribui para uma experiência do usuário mais rica e adaptável.

--- components/MainFooter.vue
+++ components/MainFooter.vue
@@ -1,7 +1,13 @@
 <template>
-    <div class="container max-w-4xl mx-auto py-6 px-6 mt-auto"></div>
+    <div class="w-full mx-auto mt-auto items-center justify-center overflow-hidden relative">
+        <div class="bg-gradient-to-b from-transparent to-white dark:to-black opacity-95 absolute inset-0 h-full z-10"></div>
+        <div class="bg-gradient-to-r from-left to-right from-white to-white dark:from-black dark:to-black dark:via-transparent via-transparent opacity-95 absolute inset-0 h-full z-10"></div>
+        <img :src="isDarkMode ? '/github-user-contribution-dark.svg' : '/github-user-contribution.svg'" alt="github-user-contribution" class="w-full transform scale-105 transition-all duration-300 md:-mb-24 z-0">
+    </div>
 </template>
 
 <script setup>
+import { useState } from '#app';
 
+const isDarkMode = useState('isDarkMode', () => false);
 </script>
